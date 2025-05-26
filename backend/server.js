6644e36633bb5c4deec62c96106dbf5d2233efb2
Zawad Arefin <syed.arefin@education.nsw.gov.au>
2025-05-26 14:15:17 +1000

    started sql connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,36 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const sqlite3 = require("sqlite3").verbose();
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const { title } = require('process');
+
 
 const app = express()
 const port = 3000
 
-app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../frontend")));
-console.log(path.join(__dirname, "../frontend"));
+app.use(express.json());
+app.use(cors())
+
+
+const dbPath = path.join(__dirname, "database", 'database.db');
+app.use("/", express.static(path.join(__dirname, "../frontend")));
+
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error("Error opening database:", err)
+    } else {
+        console.log("Connected to database");
+        db.run(`CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT
+            username TEXT,
+            email TEXT,
+            password TEXT,
+            role TEXT
+            )`);
+    }
+})
 
 app.get('/', (req, res) => {
     res.send("hello world")
