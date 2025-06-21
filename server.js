b4d32cbc27adc14f7b384e6fa063ba92f85420ac
root <root@Zawad.>
2025-06-21 16:17:36 +1000

    login functionality

--- server.js
+++ server.js
@@ -17,18 +17,39 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
 })
 
+// Email Regex check
+
+
+// Password Regex check
+
+
+// Register
 router.post('/register', (req, res) => {
     const { username, password, role } = req.body;
     const hash = bcrypt.hashSync(password, 10);
-    db.run("INSERT INTO users (username, password, role) VALUES (?, ?, ?)") [username, hash, role], err => {
+    db.run("INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)") [username, email, hash, role], err => {
         if (err) return res.status(400).json({
-            error: "Username already exists"
+            error: "Username or email already exists"
         });
         res.json({
             message: "Registered Successfully"
         });
     }
 })
+
+// Login
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    db.get('SELECT * FROM users WHERE username = ?', [username], (err, user) => {
+        if (err || !user || !bcrypt.compareSync(password, user.password)) {
+            return res.status(401).json({ error: "Invalid Credentials "})
+        }
+        req.session.user = { id: user.id, role: user.role };
+        res.json({ message: "Logged in", role: user.role })
+    })
+})
+
+
 
 
 app.use(session({
