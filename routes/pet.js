52eee70d62cf502ac7206f17ce875f32a6c14c42
zawadarefin <zawadarefin0@gmail.com>
2025-06-21 16:45:19 +1000

    creating and viewing posts

---
+++ routes/pet.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const db = require('../db/database');
+const router = express.Router();
+
+// Pet owner adding a post
+router.post('add', (req, res) => {
+    if (req.session.user?.role !== 'owner') return res.sendStatus(403)
+    const { pet_name, pet_age, description, image_url } = req.body;
+    db.run('INSERT INTO posts (user_id, pet_name, pet_age description, image_url) VALUES (?, ?, ?, ?)',
+        [req.session.user.id, pet_name, pet_age, description, image_url], 
+        err => {
+            if (err) return res.status(500).json({ error: err.message })
+            res.json({ message: "Post created" })
+        }
+    )
+})
+
+// Viewing posts 
+router.get('all', (req, res) => {
+    db.all("SELECT posts.*, users.username FROM post JOIN users on posts.user_id = users.id", [], (err, posts) => {
+        if (err) return res.status(500).json({ error: err.message })
+            res.json(posts)
+    })
+})
+
+module.exports = router;
