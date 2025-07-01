d48b5264804078371c7352533afe382737fc3d8a
zawadarefin0 <zawadarefin0@gmail.com>
2025-07-01 23:20:25 +1000

    removed comments

--- routes/auth.js
+++ routes/auth.js
@@ -58,16 +58,3 @@ router.post('/logout', (req, res) => {
 });
 
 module.exports = router;
-
-
-// const container = document.querySelector('.container')
-// const registerBtn = document.querySelector('.register-btn')
-// const loginBtn = document.querySelector('.login-btn')
-
-// registerBtn.addEventListener('click', () => {
-//     container.classList.add('active')
-// });
-
-// loginBtn.addEventListener('click', () => {
-//     container.classList.remove('active')
-// });
